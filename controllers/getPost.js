19c5bd1ab4f4d34cf151eed192a370860b922e85
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-07-26 13:55:41 +0530

    fix 2

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -2,7 +2,7 @@ const Post = require('../dataBase/models/post')
 
 module.exports = async (req, res) => {
     try {
-        const post = await (await Post.findById(_id = req.query.id)).populate('author')
+        const post = await (await Post.findById(_id = req.query.id)).populate('author', 'username')
         res.render('post', { post })
     } catch (error) {
         console.error(error.message)
