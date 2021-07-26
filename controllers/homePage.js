e865aa7eba47246743a8bddcd34a51ddf0f51e82
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-07-26 13:49:14 +0530

    ,

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -2,7 +2,7 @@ const Post = require('../dataBase/models/post')
 
 module.exports = async (req, res) => {
     try {
-        const posts = await (await Post.find({})).populate('author')
+        const posts = await (await Post.find({}))
         res.render('index', { posts })
     } catch (error) {
         console.error(error.message)
