8f2f293c9bccf25957cca0a3b675680e78897c87
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-07-26 13:52:18 +0530

    fix

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -2,7 +2,7 @@ const Post = require('../dataBase/models/post')
 
 module.exports = async (req, res) => {
     try {
-        const posts = await (await Post.find({}))
+        const posts = await (await Post.find({})).populate('author')
         res.render('index', { posts })
     } catch (error) {
         console.error(error.message)
